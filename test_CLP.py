4c5c81c88e859e10d4de314be8979ec9d4a92e17
Pytho7643 <pythonic.764309@gmail.com>
2025-06-18 15:53:40 +0300

    Add files via upload

---
+++ test_CLP.py
@@ -0,0 +1,14 @@
+import unittest
+import CLP_sorter
+
+class Test_CLP_sorter(unittest.TestCase):
+
+    def test_sort(self):
+        list = ['1', '2', '3']
+        expected = ['3', '2', '1']
+
+        result = CLP_sorter.sort_lines(list, reverse=True)
+        self.assertEqual(result, expected)
+
+if __name__ == "__main__":
+    unittest.main()
